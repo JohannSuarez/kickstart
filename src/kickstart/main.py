2e81b620d6cbf2f6362425d0781299cd564e06e5
Matt Williams <mattinzhuhai@outlook.com>
2021-09-30 15:04:12 -0700

    :octocat: Added command line args

--- src/kickstart/main.py
+++ src/kickstart/main.py
@@ -2,43 +2,31 @@
 Example Driver code
 """
 # standard lib
-import argparse
 
 # package
 from kickstart import Config
 from kickstart.logger import pkg_logger as pl
+from kickstart.command_line import Cli
 
 # create the logger at module level
-logger = pl.Logger().get_logger()
+Log = pl.Logger().get_logger()
 
 
 def run() -> None:
     """
     Example function to execute through poetry scripts
     """
-    parser = argparse.ArgumentParser(
-        prog="kickstart",
-        usage="%(prog)s [options]",
-        description="Example package",
-        allow_abbrev=False,
-    )
-    parser.add_argument(
-        "-r",
-        "--run",
-        action="store_true",
-    )
-
-    args = parser.parse_args()
-    if args.run:
-        logger.debug('Received -r as a command line argument')
+    cli = Cli()
+    if cli.args.run:
+        Log.debug('Received -r as a command line argument')
 
     try:
-        logger.info("APP_ENV: %s :: package name: %s@%s",
+        Log.info("APP_ENV: %s :: package name: %s@%s",
                     Config.env(), Config.package(), Config.version())
     except KeyError as error:
-        logger.error(
+        Log.error(
             'Could not find %s in .env file. Please consult the README', error)
-        logger.info('Testing for %s@%s', Config.package(), Config.version())
-        logger.debug('Testing for %s@%s', Config.package(), Config.version())
-        logger.warning('Testing for %s@%s', Config.package(), Config.version())
-        logger.error('Testing for %s@%s', Config.package(), Config.version())
+        Log.info('Testing for %s@%s', Config.package(), Config.version())
+        Log.debug('Testing for %s@%s', Config.package(), Config.version())
+        Log.warning('Testing for %s@%s', Config.package(), Config.version())
+        Log.error('Testing for %s@%s', Config.package(), Config.version())
